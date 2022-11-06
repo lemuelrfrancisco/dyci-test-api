1df50d0c40f490c9800f8bc1adf9cca757f5ba24
Lemuel Francisco <lemuelrfrancisco@gmail.com>
2022-11-06 10:19:27 +0800

    Disable check auth

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,116 +1,116 @@
-const Post = require( "../models/post" );
+const Post = require('../models/post');
 
-exports.createPost = ( req, res, next ) => {
-  const url = req.protocol + "://" + req.get( "host" );
-  const post = new Post( {
+exports.createPost = (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
+  const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  } );
+    imagePath: url + '/images/' + req.file.filename,
+    creator: req.userData.userId,
+  });
   post
     .save()
-    .then( createdPost => {
-      res.status( 201 ).json( {
-        message: "Post added successfully",
+    .then((createdPost) => {
+      res.status(201).json({
+        message: 'Post added successfully',
         post: {
           ...createdPost,
-          id: createdPost._id
-        }
-      } );
-    } )
-    .catch( error => {
-      res.status( 500 ).json( {
-        message: "Creating a post failed!"
-      } );
-    } );
+          id: createdPost._id,
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Creating a post failed!',
+      });
+    });
 };
 
-exports.updatePost = ( req, res, next ) => {
+exports.updatePost = (req, res, next) => {
   let imagePath = req.body.imagePath;
-  if ( req.file ) {
-    const url = req.protocol + "://" + req.get( "host" );
-    imagePath = url + "/images/" + req.file.filename;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    imagePath = url + '/images/' + req.file.filename;
   }
-  const post = new Post( {
+  const post = new Post({
     _id: req.body.id,
     title: req.body.title,
     content: req.body.content,
     imagePath: imagePath,
-    creator: req.userData.userId
-  } );
-  Post.updateOne( { _id: req.params.id, creator: req.userData.userId }, post )
-    .then( result => {
-      console.log( result );
-      if ( result.modifiedCount > 0 ) {
-        res.status( 200 ).json( { message: "Update successful!" } );
+    creator: req.userData.userId,
+  });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    .then((result) => {
+      console.log(result);
+      if (result.modifiedCount > 0) {
+        res.status(200).json({ message: 'Update successful!' });
       } else {
-        res.status( 401 ).json( { message: "Not authorized!" } );
+        res.status(401).json({ message: 'Not authorized!' });
       }
-    } )
-    .catch( error => {
-      res.status( 500 ).json( {
-        message: "Couldn't udpate post!"
-      } );
-    } );
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Couldn't udpate post!",
+      });
+    });
 };
 
-exports.getPosts = ( req, res, next ) => {
+exports.getPosts = (req, res, next) => {
   const pageSize = +req.query.pagesize;
   const currentPage = +req.query.page;
   const postQuery = Post.find();
   let fetchedPosts;
-  if ( pageSize && currentPage ) {
-    postQuery.skip( pageSize * ( currentPage - 1 ) ).limit( pageSize );
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
   postQuery
-    .then( documents => {
+    .then((documents) => {
       fetchedPosts = documents;
       return Post.count();
-    } )
-    .then( count => {
-      res.status( 200 ).json( {
-        message: "Posts fetched successfully!",
+    })
+    .then((count) => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
         posts: fetchedPosts,
-        maxPosts: count
-      } );
-    } )
-    .catch( error => {
-      res.status( 500 ).json( {
-        message: "Fetching posts failed!"
-      } );
-    } );
+        maxPosts: count,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Fetching posts failed!',
+      });
+    });
 };
 
-exports.getPost = ( req, res, next ) => {
-  Post.findById( req.params.id )
-    .then( post => {
-      if ( post ) {
-        res.status( 200 ).json( post );
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
       } else {
-        res.status( 404 ).json( { message: "Post not found!" } );
+        res.status(404).json({ message: 'Post not found!' });
       }
-    } )
-    .catch( error => {
-      res.status( 500 ).json( {
-        message: "Fetching post failed!"
-      } );
-    } );
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Fetching post failed!',
+      });
+    });
 };
 
-exports.deletePost = ( req, res, next ) => {
-  Post.deleteOne( { _id: req.params.id, creator: req.userData.userId } )
-    .then( result => {
-      console.log( result );
-      if ( result.deletedCount > 0 ) {
-        res.status( 200 ).json( { message: "Deletion successful!" } );
+exports.deletePost = (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then((result) => {
+      console.log(result);
+      if (result.deletedCount > 0) {
+        res.status(200).json({ message: 'Deletion successful!' });
       } else {
-        res.status( 401 ).json( { message: "Not authorized!" } );
+        res.status(401).json({ message: 'Not authorized!' });
       }
-    } )
-    .catch( error => {
-      res.status( 500 ).json( {
-        message: "Deleting posts failed!"
-      } );
-    } );
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Deleting posts failed!',
+      });
+    });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,20 +1,23 @@
-const express = require("express");
+const express = require('express');
 
-const PostController = require("../controllers/posts");
+const PostController = require('../controllers/posts');
 
-const checkAuth = require("../middleware/check-auth");
-const extractFile = require("../middleware/file");
+const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 
 const router = express.Router();
 
-router.post("", checkAuth, extractFile, PostController.createPost);
+router.post('', extractFile, PostController.createPost);
+// router.post("", checkAuth, extractFile, PostController.createPost);
 
-router.put("/:id", checkAuth, extractFile, PostController.updatePost);
+router.put('/:id', extractFile, PostController.updatePost);
+// router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
-router.get("", PostController.getPosts);
+router.get('', PostController.getPosts);
 
-router.get("/:id", PostController.getPost);
+router.get('/:id', PostController.getPost);
 
-router.delete("/:id", checkAuth, PostController.deletePost);
+router.delete('/:id', PostController.deletePost);
+// router.delete("/:id", checkAuth, PostController.deletePost);
 
 module.exports = router;
