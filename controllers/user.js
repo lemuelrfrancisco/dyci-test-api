e3f4fea392809f3dcb80477e5420ccdac38bbe80
Lemuel Francisco <lemuelrfrancisco@gmail.com>
2022-11-08 12:56:07 +0800

    update user controller

--- controllers/user.js
+++ controllers/user.js
@@ -30,12 +30,13 @@ exports.createUser = (req, res, next) => {
 };
 
 exports.userLogin = (req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'POST');
   let fetchedUser;
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
         return res.status(401).json({
-          message: 'Auth failed',
+          message: 'Auth failed. Incorrect Email or password.',
         });
       }
       fetchedUser = user;
@@ -48,20 +49,24 @@ exports.userLogin = (req, res, next) => {
         });
       }
       const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
+        {
+          email: fetchedUser.email,
+          userId: fetchedUser._id,
+          role: fetchedUser.role,
+        },
         process.env.JWT_KEY,
-        { expiresIn: '24h' }
+        { expiresIn: '3h' }
       );
+      res.header('token', token);
       res.status(200).json({
         token: token,
-        expiresIn: 3600 * 24,
         userId: fetchedUser._id,
+        expiresIn: 10800,
       });
     })
     .catch((err) => {
-      console.log(err);
       return res.status(401).json({
-        message: 'Invalid authentication credentials!',
+        message: 'Auth failed. Incorrect Email or password.',
       });
     });
 };
